2cb554d45000b8ea9019e82c322f324571bbb193
Gabrielle <gfulk@trinity.edu>
2022-10-30 14:21:04 -0500

    Car prefabs start moving on button click and stop when they hit wall

---
+++ FroggerStopper/Assets/Scripts/CarMovements.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovements : MonoBehaviour
+{
+    public float carSpeed;
+    private Rigidbody2D rb;
+    private GameObject carStopper;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        carStopper = GameObject.Find("CarStopper");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TriggerCarStart()
+    {
+        rb.velocity = new Vector2(-carSpeed, 0);
+    }
+
+    private void TriggerCarStop()
+    {
+        rb.velocity = new Vector2(0,0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "CarStopper") 
+        {
+            TriggerCarStop();
+        }
+    }
+}
