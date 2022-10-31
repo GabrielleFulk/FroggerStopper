9a26a070a173b0a5764c7f4162e3dafa8123bd71
Jack <jpittman@trinity.edu>
2022-10-31 13:20:43 -0500

    Small Changes starting the main game script

---
+++ FroggerStopper/Assets/Scripts/MainGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour
+{
+
+    private List<string> lane1 = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
