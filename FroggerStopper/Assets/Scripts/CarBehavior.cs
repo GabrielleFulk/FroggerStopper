4fb2a327614b84b609dfc0d82ba7823a7fb95440
Nicolas Diaz <nicolasd1018@gmail.com>
2022-11-09 13:22:02 -0600

    Drag and Drop Move work

--- FroggerStopper/Assets/Scripts/CarBehavior.cs
+++ FroggerStopper/Assets/Scripts/CarBehavior.cs
@@ -1,67 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using System.Runtime.CompilerServices;
 using Unity.Burst.CompilerServices;
-=======
->>>>>>> ec67dc14d9f908c77393c6b287afec27af6bcc59
 using UnityEngine;
 
 public class CarBehavior : MonoBehaviour
 {
-<<<<<<< HEAD
     private bool snap;
     private Collider2D slot;
     private bool move;
+    private bool go;
     // Start is called before the first frame update
     void Start()
     {
         move = true;
-=======
-    private UserInput userInput;
-    private bool snap;
-    private Collider2D slot;
-    // Start is called before the first frame update
-    void Start()
-    {
-        userInput = FindObjectOfType<UserInput>();
->>>>>>> ec67dc14d9f908c77393c6b287afec27af6bcc59
         snap = false;
+        go = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        if (snap && !move)
+        if (snap && !move && !go)
         {
             transform.position = slot.transform.position;
 
         }
-        if (!move & !snap) Destroy(gameObject);
+        if (!move & !snap && !go) Destroy(gameObject);
         if (move) transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, transform.position.z);
     }
     private void OnTriggerEnter2D(Collider2D collider)
     {
         Debug.Log("Collide");
-=======
-       
-        
-        if(snap && !Input.GetMouseButton(0))
-        {
-            transform.position = slot.transform.position;
-            snap = false;
-        }
-        
-    }
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
->>>>>>> ec67dc14d9f908c77393c6b287afec27af6bcc59
         if (collider.gameObject.tag == "slot")
         {
             snap = true;
             slot = collider;
-<<<<<<< HEAD
             Debug.Log("Collide 2");
         }
 
@@ -80,9 +54,6 @@ public class CarBehavior : MonoBehaviour
     public void setMove(bool c) { move = c; }
 
     public bool getMove() { return move; }
+
+    public void setGo(bool c) { go = c; }
 }
-=======
-        }
-    }
-}
->>>>>>> ec67dc14d9f908c77393c6b287afec27af6bcc59
--- FroggerStopper/Assets/Scripts/UserInput.cs
+++ FroggerStopper/Assets/Scripts/UserInput.cs
@@ -1,46 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
-<<<<<<< HEAD
 using Unity.VisualScripting;
-=======
->>>>>>> ec67dc14d9f908c77393c6b287afec27af6bcc59
 using UnityEngine;
 
 public class UserInput : MonoBehaviour
 {
     // Start is called before the first frame update
-<<<<<<< HEAD
     public GameObject car;
     void Start()
     {
 
-=======
-    bool move = true;
-    public GameObject car;
-    void Start()
-    {
-        
->>>>>>> ec67dc14d9f908c77393c6b287afec27af6bcc59
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         if (Input.GetMouseButtonDown(0))
         {
-=======
-
-        if (Input.GetMouseButton(0))
-        {
-            move = true;
->>>>>>> ec67dc14d9f908c77393c6b287afec27af6bcc59
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
             if (hit && hit.collider.CompareTag("Car"))
             {
-<<<<<<< HEAD
                 Debug.Log("hit");
                 hit.collider.gameObject.GetComponent<CarBehavior>().setMove(!hit.collider.gameObject.GetComponent<CarBehavior>().getMove());
             }
@@ -48,18 +29,6 @@ public class UserInput : MonoBehaviour
     }
 
 
-=======
-                //Debug.Log("hit");
-                hit.collider.gameObject.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, hit.collider.gameObject.transform.position.z);
-            }
-        }
-        else if (Input.GetMouseButtonUp(0)) { move = false; }
-    }
-
-    public void setMove(bool c) { move = c; }
-
-    public bool getMove() {return move; }
->>>>>>> ec67dc14d9f908c77393c6b287afec27af6bcc59
 
     public void SpawnCar()
     {
