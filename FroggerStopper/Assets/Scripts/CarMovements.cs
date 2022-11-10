8beb3f74c3db53ccd78a96ca362c601337242259
Gabrielle <gfulk@trinity.edu>
2022-11-10 16:05:06 -0600

    Add car images and differing speeds/points. Add point/speed text

--- FroggerStopper/Assets/Scripts/CarMovements.cs
+++ FroggerStopper/Assets/Scripts/CarMovements.cs
@@ -7,16 +7,27 @@ public class CarMovements : MonoBehaviour
     public float carSpeed;
     private Rigidbody2D rb;
     private GameObject carStopper;
+    private UserInput userInput;
+    private bool snap;
+    private Collider2D slot;
+    public int points;
+    public Sprite carImage;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         carStopper = GameObject.Find("CarStopper");
+        userInput = FindObjectOfType<UserInput>();
+        snap = false;
     }
 
     void Update()
     {
-        
+        if(snap && !Input.GetMouseButton(0))
+        {
+            transform.position = slot.transform.position;
+            snap = false;
+        }
     }
 
     public void TriggerCarStart()
@@ -24,11 +35,17 @@ public class CarMovements : MonoBehaviour
         rb.velocity = new Vector2(-carSpeed, 0);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collision.gameObject.tag == "CarStopper") 
+        if (collider.gameObject.tag == "CarStopper") 
         {
             Destroy(gameObject);
+        }
+
+        if (collider.gameObject.tag == "slot")
+        {
+            snap = true;
+            slot = collider;
         }
     }
 }
--- FroggerStopper/Assets/Scripts/UserInput.cs
+++ FroggerStopper/Assets/Scripts/UserInput.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 
 public class UserInput : MonoBehaviour
 {
-    // Start is called before the first frame update
     bool move = true;
     public GameObject car;
+    public Sprite purpleCar;
+    public Sprite dumpTruck;
+    public Sprite redCar;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         if (Input.GetMouseButton(0))
         {
             move = true;
@@ -35,9 +36,27 @@ public class UserInput : MonoBehaviour
 
     public bool getMove() {return move; }
 
-    public void SpawnCar()
-    {
+    public void SpawnRedCar()
+    {        
+        car.GetComponent<SpriteRenderer>().sprite = redCar;
+        car.GetComponent<CarMovements>().points = 3;
+        car.GetComponent<CarMovements>().carSpeed = 2;
         Instantiate(car, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0.0f), Quaternion.identity);
-        Debug.Log("spawning car");
+    }
+
+    public void SpawnPurpleCar()
+    {
+        car.GetComponent<SpriteRenderer>().sprite = purpleCar;
+        car.GetComponent<CarMovements>().points = 7;
+        car.GetComponent<CarMovements>().carSpeed = 4;
+        Instantiate(car, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0.0f), Quaternion.identity);
+    }
+
+    public void SpawnDumpTruck()
+    {
+        car.GetComponent<SpriteRenderer>().sprite = dumpTruck;
+        car.GetComponent<CarMovements>().points = 5;
+        car.GetComponent<CarMovements>().carSpeed = 1;
+        Instantiate(car, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0.0f), Quaternion.identity);
     }
 }
