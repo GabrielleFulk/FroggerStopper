761947aca6e26619396f1dd560735f9555a49ff9
Gabrielle <gfulk@trinity.edu>
2022-11-11 12:56:53 -0600

    pushing my changes again

--- FroggerStopper/Assets/Scripts/ButtonController.cs
+++ FroggerStopper/Assets/Scripts/ButtonController.cs
@@ -8,7 +8,6 @@ public class ButtonController : MonoBehaviour
 {
     private CarMovements[] allCarScripts;
     private FrogMovement[] allFrogScripts;
-    private CarBehavior[] allCarBehavior;
     public Button PlayButton;
     public Button CarButton;
 
