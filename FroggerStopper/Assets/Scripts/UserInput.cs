ebc0180105f8326a23c8411070c5fd0a1e2828c2
Gabrielle <gfulk@trinity.edu>
2022-11-30 13:06:44 -0600

    Add point description and distinction to win pop ups

--- FroggerStopper/Assets/Scripts/UserInput.cs
+++ FroggerStopper/Assets/Scripts/UserInput.cs
@@ -54,7 +54,7 @@ public class UserInput : MonoBehaviour
     public void SpawnRedCar()
     {        
         car.GetComponent<SpriteRenderer>().sprite = redCar;
-        car.GetComponent<CarMovements>().points = 3;
+        car.GetComponent<CarMovements>().points = 5;
         car.GetComponent<CarMovements>().carSpeed = 2;
         Instantiate(car, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0.0f), Quaternion.identity);
     }
@@ -62,7 +62,7 @@ public class UserInput : MonoBehaviour
     public void SpawnPurpleCar()
     {
         car.GetComponent<SpriteRenderer>().sprite = purpleCar;
-        car.GetComponent<CarMovements>().points = 7;
+        car.GetComponent<CarMovements>().points = 4;
         car.GetComponent<CarMovements>().carSpeed = 4;
         Instantiate(car, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0.0f), Quaternion.identity);
     }
@@ -70,7 +70,7 @@ public class UserInput : MonoBehaviour
     public void SpawnDumpTruck()
     {
         car.GetComponent<SpriteRenderer>().sprite = dumpTruck;
-        car.GetComponent<CarMovements>().points = 5;
+        car.GetComponent<CarMovements>().points = 6;
         car.GetComponent<CarMovements>().carSpeed = 1;
         Instantiate(car, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0.0f), Quaternion.identity);
     }
